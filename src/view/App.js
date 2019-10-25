d9cb6cf903b8727d599f9bc3324c3654ea923351
jasonYoge <jasonYoga@github.com>
2019-10-25 15:05:35 +0800

    bugfix: fixed setState bug

--- src/view/App.js
+++ src/view/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import Checkbox from 'antd/lib/checkbox';
 import Button from 'antd/lib/button';
 import * as R from 'ramda';
@@ -16,7 +16,7 @@ const setUserProp = property(R.over, 'username')(R.toUpper);
 
 const getUserProp = property(R.view, 'username');
 
-const setUsername = setState => setState.contramap(console.log).contramap(setUserProp);
+const setUsername = setState => setState.contramap(setUserProp);
 
 const getUsername = getState => getState.map(getUserProp);
 
@@ -35,7 +35,7 @@ function useFormState(initState) {
 function App() {
   const [setState, getState] = useFormState(initState);
 
-  getState.map(console.log).runWith();
+  getState.map(R.concat('username: ')).map(console.log).runWith();
 
   const onSubmit = handler(setState);
 
