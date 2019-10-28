942af5dde05b1af92fe62d1d5fed5387c2e707e4
jasonYoge <jasonYoga@github.com>
2019-10-28 19:13:48 +0800

    bugfix

--- src/view/App.js
+++ src/view/App.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import Checkbox from 'antd/lib/checkbox';
 import Button from 'antd/lib/button';
 import * as R from 'ramda';
 import Arrow from 'crocks/Arrow';
 import Async from 'crocks/Async';
 import maybeToAsync from 'crocks/Async/maybeToAsync';
-import { Just, Nothing } from 'crocks/Maybe';
 import safe from 'crocks/Maybe/safe';
 import isObject from 'crocks/predicates/isObject';
 
@@ -16,7 +15,7 @@ import Form from './components/Form';
 import './App.css';
 
 const requestUrl = 'https://gist.githubusercontent.com/jasonYoge/\
-  39a229d8fb21e31a31650ebff27928c8/raw/37ffce0e4cb3ac1d02655ef4010c3b1403f55f1c/loginInfo.json';
+39a229d8fb21e31a31650ebff27928c8/raw/37ffce0e4cb3ac1d02655ef4010c3b1403f55f1c/loginInfo.json';
 
 const property = R.curry((f, x) => f(R.lensProp(x)));
 
@@ -44,7 +43,7 @@ const useFormState = initState => {
   return [setUsername(setState), getUsername(getState)];
 };
 
-const useFetch = R.curry((url, setState) => useEffect(() => fetchMethod(url).fork(() => {}, (res) => setState.runWith(res)), []));
+const useFetch = R.curry((url, setState) => useEffect(() => fetchMethod(url).fork(alert, (res) => setState.runWith(res)), []));
 
 function App() {
   const [setState, getState] = useFormState(initState);
