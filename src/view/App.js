d439a341c0486a021da2b688fb91a90da1598dbd
jasonYoge <jasonYoga@github.com>
2019-10-28 18:48:19 +0800

    feat: 提交

--- src/view/App.js
+++ src/view/App.js
@@ -3,12 +3,20 @@ import Checkbox from 'antd/lib/checkbox';
 import Button from 'antd/lib/button';
 import * as R from 'ramda';
 import Arrow from 'crocks/Arrow';
+import Async from 'crocks/Async';
+import maybeToAsync from 'crocks/Async/maybeToAsync';
+import { Just, Nothing } from 'crocks/Maybe';
+import safe from 'crocks/Maybe/safe';
+import isObject from 'crocks/predicates/isObject';
 
 import { TextInput, PwdInput } from './components/Input';
 import initState from './State';
 import Form from './components/Form';
 
 import './App.css';
+
+const requestUrl = 'https://gist.githubusercontent.com/jasonYoge/\
+  39a229d8fb21e31a31650ebff27928c8/raw/37ffce0e4cb3ac1d02655ef4010c3b1403f55f1c/loginInfo.json';
 
 const property = R.curry((f, x) => f(R.lensProp(x)));
 
@@ -22,7 +30,11 @@ const getUsername = getState => getState.map(getUserProp);
 
 const handler = R.curry((setState, data) => setState.runWith(data));
 
-function useFormState(initState) {
+const fetchMethod = url => Async((reject, resolve) => fetch(url).then(data => data.json().then(resolve)).catch(reject))
+  // .chain(res => Async((err, data) => res.json().then(data).catch(err)))
+  .chain(maybeToAsync('Fetch error!!!', safe(isObject)));
+
+const useFormState = initState => {
   const [state, _setState] = useState(initState);
 
   const getState = Arrow(() => state);
@@ -30,10 +42,14 @@ function useFormState(initState) {
   const setState = Arrow(_setState);
 
   return [setUsername(setState), getUsername(getState)];
-}
+};
+
+const useFetch = R.curry((url, setState) => useEffect(() => fetchMethod(url).fork(() => {}, (res) => setState.runWith(res)), []));
 
 function App() {
   const [setState, getState] = useFormState(initState);
+
+  useFetch(requestUrl, setState);
 
   getState.map(R.concat('username: ')).map(console.log).runWith();
 
